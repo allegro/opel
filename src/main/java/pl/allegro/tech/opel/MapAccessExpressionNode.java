4ed31b49bc847856f8ed75cec94ecdb8240e78e0
Krzysztof Kwiatkowski <kwiatkk1@gmail.com>
2019-03-20 12:34:03 +0100

    remove 'cant extract value for key' log

--- src/main/java/pl/allegro/tech/opel/MapAccessExpressionNode.java
+++ src/main/java/pl/allegro/tech/opel/MapAccessExpressionNode.java
@@ -1,17 +1,11 @@
 package pl.allegro.tech.opel;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.lang.invoke.MethodHandles;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class MapAccessExpressionNode implements OpelNode {
-    private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
     private final OpelNode subject;
     private final OpelNode fieldName;
 
@@ -28,7 +22,6 @@ public class MapAccessExpressionNode implements OpelNode {
     private CompletableFuture<?> extractValueFromListOrMap(CompletableFuture<?> obj, CompletableFuture<?> key) {
         return obj.thenCombine(key, (it, k) -> {
             if (it == null) {
-                logger.info("Can't extract value for key '" + k + "' from null");
                 return null;
             }
             if (it instanceof Map) {

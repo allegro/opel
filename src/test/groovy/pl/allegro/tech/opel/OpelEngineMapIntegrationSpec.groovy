f7e44ad2bb934b5d5398a4c3d89c64ec3336d0b9
Krzysztof Bielicki <krzysztof.bielicki@allegro.pl>
2020-03-20 14:42:22 +0100

    Map with null value cannot be created, null values are filtered out.

--- src/test/groovy/pl/allegro/tech/opel/OpelEngineMapIntegrationSpec.groovy
+++ src/test/groovy/pl/allegro/tech/opel/OpelEngineMapIntegrationSpec.groovy
@@ -17,11 +17,12 @@ class OpelEngineMapIntegrationSpec extends Specification {
         engine.eval(input).get() == new HashMap(expResult)
 
         where:
-        input                || expResult
-        "{:}"                || [:]
-        "{'x':2}"            || [x: 2]
-        "({'x': 2 })"        || [x: 2]
-        "{'x': 2, 'y':3 }"   || [x: 2, y: 3]
+        input                 || expResult
+        "{:}"                 || [:]
+        "{'x':2}"             || [x: 2]
+        "({'x': 2 })"         || [x: 2]
+        "{'x': 2, 'y':3 }"    || [x: 2, y: 3]
+        "{'x': 2, 'y':null }" || [x: 2]
     }
 
     @Unroll
@@ -33,9 +34,9 @@ class OpelEngineMapIntegrationSpec extends Specification {
         engine.eval(input, EvalContextBuilder.create().withValues(values).build()).get() == new HashMap(expResult)
 
         where:
-        input                           || values                                          || expResult
-        "{x: (6+7), 'y':3 }"            || [x: CompletableFuture.completedFuture(2.0)]     || [(2.0): 13, y: 3]
-        "{x: 6+7, 'y':3 }"              || [x: CompletableFuture.completedFuture(2.0)]     || [(2.0): 13, y: 3]
+        input                || values                                      || expResult
+        "{x: (6+7), 'y':3 }" || [x: CompletableFuture.completedFuture(2.0)] || [(2.0): 13, y: 3]
+        "{x: 6+7, 'y':3 }"   || [x: CompletableFuture.completedFuture(2.0)] || [(2.0): 13, y: 3]
     }
 
     @Unroll
@@ -47,12 +48,12 @@ class OpelEngineMapIntegrationSpec extends Specification {
         engine.eval(input).get() == expResult
 
         where:
-        input                              || expResult
-        "{(1+1.5): 2, 'y':3 }"             || [2.5: 2, y: 3]
-        "{('x' + 'x'):2}"                  || [xx: 2]
-        "{[].size():2}"                    || [0: 2]
-        "{([].size()):2}"                  || [0: 2]
-        "{(['1'].size()):['1'].size()}"    || [1: 1]
+        input                           || expResult
+        "{(1+1.5): 2, 'y':3 }"          || [2.5: 2, y: 3]
+        "{('x' + 'x'):2}"               || [xx: 2]
+        "{[].size():2}"                 || [0: 2]
+        "{([].size()):2}"               || [0: 2]
+        "{(['1'].size()):['1'].size()}" || [1: 1]
     }
 
     @Unroll
@@ -64,64 +65,64 @@ class OpelEngineMapIntegrationSpec extends Specification {
         engine.eval(input).get() == expResult
 
         where:
-        input                                    || expResult
-        "val x = {('x' + 'x'):2}; x.xx"          || 2
-        "val x = {('x' + 'x'):2}; x.get('xx')"   || 2
-        "val x = {('x' + 'x'):2}; x.xx"          || 2
-        "val x = {('x' + 'x'):2}; x['xx']"       || 2
+        input                                  || expResult
+        "val x = {('x' + 'x'):2}; x.xx"        || 2
+        "val x = {('x' + 'x'):2}; x.get('xx')" || 2
+        "val x = {('x' + 'x'):2}; x.xx"        || 2
+        "val x = {('x' + 'x'):2}; x['xx']"     || 2
     }
 
     @Unroll
     def 'should access map with [] notation'() {
         given:
         def engine = create()
-                .withCompletedValue('aMap', ['a' : 'x', 'b' : 'y', 'c' : 'z', (false) : 'xyz'])
+                .withCompletedValue('aMap', ['a': 'x', 'b': 'y', 'c': 'z', (false): 'xyz'])
                 .build()
 
         expect:
         engine.eval(input).get() == expResult
 
         where:
-        input                   || expResult
-        "{'x':2, 'y':3}['x']"   || 2
-        "aMap['b']"             || 'y'
-        "aMap[true == false]"   || 'xyz'
+        input                 || expResult
+        "{'x':2, 'y':3}['x']" || 2
+        "aMap['b']"           || 'y'
+        "aMap[true == false]" || 'xyz'
     }
 
     @Unroll
     def 'should access map with dot (.) notation'() {
         given:
         def engine = create()
-                .withCompletedValue('aMap', ['a' : 'x', 'b' : 'y', 'c' : 'z'])
+                .withCompletedValue('aMap', ['a': 'x', 'b': 'y', 'c': 'z'])
                 .build()
 
         expect:
         engine.eval(input).get() == expResult
 
         where:
-        input                          || expResult
-        "{'x':2, 'y':5}.x"             || 2
-        "aMap.b"                       || 'y'
+        input              || expResult
+        "{'x':2, 'y':5}.x" || 2
+        "aMap.b"           || 'y'
     }
 
-    static def fun = { it -> it.get(0).thenApply{x -> x+x} } as OpelAsyncFunction
+    static def fun = { it -> it.get(0).thenApply { x -> x + x } } as OpelAsyncFunction
 
     @Unroll
     def 'should prefer method call over field access with function as a value (#input)'() {
         given:
         def engine = create()
-                .withCompletedValue('aMap', ['get' : fun])
+                .withCompletedValue('aMap', ['get': fun])
                 .build()
 
         expect:
         engine.eval(input).get() == expResult
 
         where:
-        input                              || expResult
-        "aMap.get('get')"                  || fun
-        "(aMap.get)('get')"                || 'getget'
-        "({'get': x->x+x}.get('get'))('g')"|| 'gg'
-        "({'get': x->x+x}.get)('get')"     || 'getget'
+        input                               || expResult
+        "aMap.get('get')"                   || fun
+        "(aMap.get)('get')"                 || 'getget'
+        "({'get': x->x+x}.get('get'))('g')" || 'gg'
+        "({'get': x->x+x}.get)('get')"      || 'getget'
     }
 
     @Unroll
